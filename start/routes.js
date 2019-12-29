106d235d357dace33714771c2029719c8dcd8bff
Juliano Sirtori <julianosirtori@gmail.com>
2019-12-29 11:59:54 -0300

    New Commit

--- start/routes.js
+++ start/routes.js
@@ -2,11 +2,23 @@
 
 const Route = use('Route')
 
-Route.post('users', 'UserController.store')
-Route.post('sessions', 'SessionController.store')
+Route.post('sessions', 'SessionController.store').validator('Session')
 
-Route.post('password', 'FogotPasswordController.store')
-Route.put('password', 'FogotPasswordController.update')
+Route.post('password', 'FogotPasswordController.store').validator('ForgotPassword')
+Route.put('password', 'FogotPasswordController.update').validator('ResetPassword')
 
 Route.get('/files/:id', 'FileController.show')
-Route.post('/files', 'FileController.store')
+
+Route.group(() => {
+  Route.post('users', 'UserController.store').validator('User')
+
+  Route.post('/files', 'FileController.store')
+
+  Route.resource('projects', 'ProjectController')
+    .apiOnly()
+    .validator(new Map([[['projects.store'], ['Project']]]))
+
+  Route.resource('projects.tasks', 'TaskController')
+    .apiOnly()
+    .validator(new Map([[['projects.tasks.store'], ['Task']]]))
+}).middleware(['auth'])
